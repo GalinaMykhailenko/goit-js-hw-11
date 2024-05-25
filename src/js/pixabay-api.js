1c5eb790112acfc18c998b7b570d4c6a67be0543
GalinaMykhailenko <pr@ars.od.ua>
2024-05-26 01:49:46 +0200

    first

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,24 @@
+export function searchImage(searchQuery) {
+    const BASE_URL = "https://pixabay.com";
+    const END_POINT = "/api/";
+    const params = new URLSearchParams({
+        key: "44052553-0e6a302568f711546d3ca79ba",
+        q: searchQuery,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+    });
+
+    const url = `${BASE_URL}${END_POINT}?${params}`;
+
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .catch(error => {
+            throw new Error(`HTTP error! status: ${error.message}`);
+        });
+}
